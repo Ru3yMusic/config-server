44460cf073a2929526ba49c5b0251e9614acd670
Franco Basilio <francolitobo@gmail.com>
2026-03-17 20:12:59 -0500

    first commit

---
+++ src/test/java/com/rubymusic/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rubymusic.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
